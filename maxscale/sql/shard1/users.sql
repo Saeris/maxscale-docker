e0e27ed43c2039a7e5df5908c4bd99f87eb66ce5
Drake Costa <drake@saeris.io>
2024-03-19 22:04:04 -0700

    switch to sharded db config

--- maxscale/sql/shard1/users.sql
+++ maxscale/sql/shard1/users.sql
@@ -5,3 +5,6 @@ GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
 
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'%' WITH GRANT OPTION;
+
+SET GLOBAL max_connections=10000;
+SET GLOBAL gtid_strict_mode=ON;
